0aeb7cf39b8ab1fc3ff48adc0872548c40ee40ba
Luiz Paulo Nascimento <luizp.nascimento@outlook.com>
2021-07-23 02:01:28 -0300

    create users resolve/query graphql

--- server/src/graphql/queries/rootQuery.ts
+++ server/src/graphql/queries/rootQuery.ts
@@ -1,8 +1,10 @@
-import { GraphQLObjectType, GraphQLString } from "graphql"
+import { GraphQLList, GraphQLObjectType, GraphQLString } from "graphql"
+import { pool } from "../.."
 import { fetchMovieByTitle } from "../../services/movies"
 import { fetchYoutubeVideos } from "../../services/videos"
 import MoviesQueryType from "./movies"
 import VideosQueryType from "./videos"
+import UserType from "../../modules/user/UserType"
 
 const RootQuery = new GraphQLObjectType({
   name: 'RootQuery',
@@ -25,6 +27,24 @@ const RootQuery = new GraphQLObjectType({
       resolve: async (parent, args) => {
         const { title } = args
         return await fetchYoutubeVideos(title)
+      }
+    },
+    users: {
+      type: GraphQLList(UserType),
+      resolve: async (parent, args) => {
+        const getAllUsers = async () => {
+          try {
+            const users = await pool.query('select * from users')
+
+            return users?.rows
+          } catch (error) {
+            console.log('error', error)
+          }
+        }
+
+        const data = await getAllUsers()
+
+        return data
       }
     }
   }
