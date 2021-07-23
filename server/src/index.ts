240aeea6ea402bace38b9b3b49ab185c6abaa268
Luiz Paulo Nascimento <luizp.nascimento@outlook.com>
2021-07-23 01:14:49 -0300

    connect to db

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,5 +1,14 @@
+import { Pool } from 'pg'
 import app from './app'
 import { config } from './config'
+
+export const pool = new Pool({
+  user: config.DB_USER,
+  host: config.DB_HOST,
+  database: config.DB_DATABASE,
+  password: config.DB_PASSWORD,
+  port: Number(config.DB_PORT) || 5432,
+})
 
 app.listen(config.PORT, () => {
   console.log(`server running at http://localhost:${config.PORT}`)
