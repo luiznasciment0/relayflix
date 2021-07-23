286f9f01dcdcc5f1a9fceadb4f21d7b6c20c5e92
Luiz Paulo Nascimento <luizp.nascimento@outlook.com>
2021-07-23 00:44:27 -0300

    pretify

--- server/src/graphql/queries/rootQuery.ts
+++ server/src/graphql/queries/rootQuery.ts
@@ -5,29 +5,29 @@ import MoviesQueryType from "./movies"
 import VideosQueryType from "./videos"
 
 const RootQuery = new GraphQLObjectType({
-    name: 'RootQuery',
-    fields: {
-        moviesByTitle: {
-            type: MoviesQueryType,
-            args: {
-                title: { type: GraphQLString },
-            },
-            resolve: async (parent, args) => {
-                const { title } = args
-                return await fetchMovieByTitle(title)
-            },
-        },
-        videosByTitle: {
-            type: VideosQueryType,
-            args: {
-                title: { type: GraphQLString },
-            },
-            resolve: async (parent, args) => {
-                const { title } = args
-                return await fetchYoutubeVideos(title)
-            }
-        }
+  name: 'RootQuery',
+  fields: {
+    moviesByTitle: {
+      type: MoviesQueryType,
+      args: {
+        title: { type: GraphQLString },
+      },
+      resolve: async (parent, args) => {
+        const { title } = args
+        return await fetchMovieByTitle(title)
+      },
+    },
+    videosByTitle: {
+      type: VideosQueryType,
+      args: {
+        title: { type: GraphQLString },
+      },
+      resolve: async (parent, args) => {
+        const { title } = args
+        return await fetchYoutubeVideos(title)
+      }
     }
+  }
 })
 
 export default RootQuery
