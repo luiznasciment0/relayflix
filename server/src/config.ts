180b1dc1a3e206d3a6ae0f09632833ed88db0c36
Luiz Paulo Nascimento <luizp.nascimento@outlook.com>
2021-07-23 00:47:48 -0300

    config database vars

--- server/src/config.ts
+++ server/src/config.ts
@@ -7,5 +7,10 @@ export const config = {
   YOUTUBE_API_KEY: process.env.YOUTUBE_API_KEY,
   API_KEY: process.env.API_KEY,
   API_HOST: process.env.API_HOST,
+  DB_USER: process.env.DB_USER,
+  DB_HOST: process.env.DB_HOST,
+  DB_DATABASE: process.env.DB_DATABASE,
+  DB_PASSWORD: process.env.DB_PASSWORD,
+  DB_PORT: process.env.DB_PORT,
   PORT: process.env.PORT || 9000,
 }
