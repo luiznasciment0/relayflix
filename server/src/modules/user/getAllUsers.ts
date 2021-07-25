1d4d55398d0f6590cd626ccb002125770423b419
Luiz Paulo Nascimento <luizp.nascimento@outlook.com>
2021-07-25 20:17:35 -0300

    improve getAllUsers method

--- server/src/modules/user/getAllUsers.ts
+++ server/src/modules/user/getAllUsers.ts
@@ -1,11 +1,28 @@
 import { pool } from '../..'
 
-export const getAllUsers = async () => {
+type User = {
+  name?: string
+  username?: string
+  email?: string
+  password?: string
+  interests?: string[]
+}
+
+export const getAllUsers = async (): Promise<{
+  users: User[] | null
+  error: Error | null
+}> => {
   try {
     const users = await pool.query('select * from users2')
 
-    return users?.rows
+    return {
+      users: users.rows,
+      error: null
+    }
   } catch (error) {
-    throw Error(`Error when getting all users: ${error}`)
+    return {
+      users: null,
+      error: Error(`Error when getting all users: ${error}`)
+    }
   }
 }
