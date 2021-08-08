1209566df41a991fe69d86c3219d101131f9973b
Luiz Paulo Nascimento <luizp.nascimento@outlook.com>
2021-08-08 01:13:54 -0300

    start nodeinterface implementation

---
+++ server/src/modules/node/nodeInterface.ts
@@ -0,0 +1,59 @@
+import { GraphQLObjectType, GraphQLTypeResolver } from 'graphql'
+import { fromGlobalId, nodeDefinitions } from 'graphql-relay'
+
+type GraphQLContext = Record<string, unknown>
+type Load = (context: GraphQLContext, id: string) => any
+type TypeLoaders = {
+  [key: string]: {
+    type: GraphQLObjectType
+    load: Load
+  }
+}
+
+const getTypeRegister = () => {
+  const typesLoaders: TypeLoaders = {}
+
+  const getTypesLoaders = () => typesLoaders
+
+  const registerTypeLoader = (type: GraphQLObjectType, load: Load) => {
+    typesLoaders[type.name] = {
+      type,
+      load
+    }
+
+    return type
+  }
+
+  const fetchById = (globalId: string, context: GraphQLContext) => {
+    const { type, id } = fromGlobalId(globalId)
+    const { load } = typesLoaders[type] || { load: null }
+
+    if (!load) return null
+
+    return load(context, id)
+  }
+
+  const typeResolver: GraphQLTypeResolver<any, GraphQLContext> = (obj) => {
+    const { type } = typesLoaders[obj.constructor.name] || { type: null }
+
+    return type
+  }
+
+  const { nodeField, nodesField, nodeInterface } = nodeDefinitions(
+    fetchById,
+    typeResolver
+  )
+
+  return {
+    registerTypeLoader,
+    getTypesLoaders,
+    nodeField,
+    nodesField,
+    nodeInterface
+  }
+}
+
+const { registerTypeLoader, nodeInterface, nodeField, nodesField } =
+  getTypeRegister()
+
+export { registerTypeLoader, nodeInterface, nodeField, nodesField }
