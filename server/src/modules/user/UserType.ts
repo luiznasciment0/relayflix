071263832e720adde20f0f965402f5c64f48585c
Luiz Paulo Nascimento <luizp.nascimento@outlook.com>
2021-08-08 22:54:08 -0300

    improve userType

--- server/src/modules/user/UserType.ts
+++ server/src/modules/user/UserType.ts
@@ -1,20 +1,62 @@
-import {
-  GraphQLInt,
-  GraphQLList,
-  GraphQLObjectType,
-  GraphQLString
-} from 'graphql'
+import { GraphQLList, GraphQLObjectType, GraphQLString } from 'graphql'
+import { globalIdField } from 'graphql-relay'
 
-const UserType = new GraphQLObjectType({
+import { GraphQLContext } from 'src/graphql/types'
+import { pool } from 'src/index'
+import { nodeInterface, registerTypeLoader } from '../node/typeRegister'
+import { getUserInterests } from './getUserInterests'
+
+export interface IUser {
+  name: string
+  username: string
+  email: string
+  password: string
+  interests?: string[]
+}
+
+export const UserType = new GraphQLObjectType({
   name: 'User',
   fields: () => ({
-    id: { type: GraphQLInt },
-    name: { type: GraphQLString },
-    username: { type: GraphQLString },
-    email: { type: GraphQLString },
+    id: globalIdField('User'),
+    name: {
+      type: GraphQLString,
+      resolve: (user) => user.name
+    },
+    username: {
+      type: GraphQLString,
+      resolve: (user) => user.username
+    },
+    email: {
+      type: GraphQLString,
+      resolve: (user) => user.email
+    },
     password: { type: GraphQLString },
-    interests: { type: GraphQLList(GraphQLString) }
-  })
+    interests: {
+      type: GraphQLList(GraphQLString),
+      resolve: async (user) => await getUserInterests(user.email)
+    }
+  }),
+  interfaces: () => [nodeInterface]
 })
 
-export default UserType
+registerTypeLoader(UserType, async (context: GraphQLContext, id: string) => {
+  if (!id) {
+    return null
+  }
+
+  try {
+    const data = await pool.query(
+      'select name, username, email, interests from users where email = ($1)',
+      [context.user?.email]
+    )
+
+    if (!data) {
+      return null
+    }
+
+    return data
+  } catch (error) {
+    console.log(`Error on registerTypeLoader UserType: ${error}`)
+    return null
+  }
+})
